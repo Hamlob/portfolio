25f6ed508b28cb83038b04a94b23d9f3ef9d54dc
Hamlob <101595484+Hamlob@users.noreply.github.com>
2024-03-16 20:33:42 +0100

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -1,36 +1,34 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "jedlo.je.good@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $subject2 = "Copy of your form submission - " . $_POST['subject'];
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+$to = "jedlo.je.good@gmail.com"; // this is your Email address
+$from = $_POST['email']; // this is the sender's Email address
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$subject2 = "Copy of your form submission - " . $_POST['subject'];
+$message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
-    // Additional headers for sending via Mailgun SMTP
-    $smtp_config = [
-        'host' => 'smtp.mailgun.org',
-        'port' => 587,
-        'username' => getenv('MAILGUN_SMTP_LOGIN'),
-        'password' => getenv('MAILGUN_SMTP_PASSWORD'),
-        'encryption' => 'tls'
-    ];
+// Additional headers for sending via Mailgun SMTP
+$smtp_config = [
+    'host' => 'smtp.mailgun.org',
+    'port' => 587,
+    'username' => getenv('MAILGUN_SMTP_LOGIN'),
+    'password' => getenv('MAILGUN_SMTP_PASSWORD'),
+    'encryption' => 'tls'
+];
 
-    //check if all fields were filled out
-    if (empty($from) || empty($name) || empty($subject) || empty($message)) {
-        echo "Please fill out all fields";
-    exit;
-    }
+//check if all fields were filled out
+if (empty($from) || empty($name) || empty($subject) || empty($message)) {
+    echo "Please fill out all fields";
+exit;
+}
 
-    // Send email
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
+// Send email
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
 
-    if (mail($to,$subject,$message,$headers) && mail($from,$subject2,$message2,$headers2)) {
-        echo "Email sent successfully!";
-    } else {
-        echo "Email sending failed";
-    }
-}   
+if (mail($to,$subject,$message,$headers) && mail($from,$subject2,$message2,$headers2)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Email sending failed";
+}
 ?>
